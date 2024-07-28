b297b80769ea90a8ce812ba08d6286077f188367
habib-hb <developerhabib1230@gmail.com>
2024-07-28 12:02:20 +0600

    new_horizon

--- routes/web.php
+++ routes/web.php
@@ -72,44 +72,44 @@ Route::get('/get_user_name', function(){
 
 
 
-// Email Varification Stuffs
-Route::get('/email/verify', function () {
-    return view('auth.verify-email');
-})->middleware('auth')->name('verification.notice');
+// // Email Varification Stuffs
+// Route::get('/email/verify', function () {
+//     return view('auth.verify-email');
+// })->middleware('auth')->name('verification.notice');
 
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
+// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+//     $request->fulfill();
 
-    return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
+//     return redirect('/home');
+// })->middleware(['auth', 'signed'])->name('verification.verify');
 
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
+// Route::post('/email/verification-notification', function (Request $request) {
+//     $request->user()->sendEmailVerificationNotification();
 
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+//     return back()->with('message', 'Verification link sent!');
+// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 
 
-// Sending Mail stuffs
-Route::get('send_mail', function () {
-    $details = [
-        'title' => 'Success',
-        'content' => 'This is an email testing using Laravel-Brevo',
-    ];
+// // Sending Mail stuffs
+// Route::get('send_mail', function () {
+//     $details = [
+//         'title' => 'Success',
+//         'content' => 'This is an email testing using Laravel-Brevo',
+//     ];
 
-    try {
-        Mail::raw('This is a test email using Brevo SMTP.', function (Message $message) {
-            $message->from('habib@valueadderhabib.com', 'Habibur Rahman');
-            $message->to('developerhabib1230@gmail.com');
-            $message->subject('Test Email');
-        });
+//     try {
+//         Mail::raw('This is a test email using Brevo SMTP.', function (Message $message) {
+//             $message->from('habib@valueadderhabib.com', 'Habibur Rahman');
+//             $message->to('developerhabib1230@gmail.com');
+//             $message->subject('Test Email');
+//         });
 
-        return 'Email sent at ' . now();
-    } catch (\Exception $e) {
-        return 'Email sending failed: ' . $e->getMessage();
-    }
-});
+//         return 'Email sent at ' . now();
+//     } catch (\Exception $e) {
+//         return 'Email sending failed: ' . $e->getMessage();
+//     }
+// });
 
 
 
@@ -178,7 +178,7 @@ Route::post('feedback', function(Request $request) {
 
         // Sending a flash message
         session()->flash('message', 'Feedback sent successfully.');
-        
+
     }
 
     // Return redirect back
